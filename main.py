b32694f0ce0125b15e7ee1c9ab8d09d07d56847f
Jingyi Mao <maojingyi9231@hotmail.com>
2025-03-02 22:50:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,82 +6,86 @@ import time
 from prophet import Prophet
 import matplotlib.pyplot as plt
 
-# Page config
-st.set_page_config(page_title="Stock Price Forecasting", page_icon=":chart_with_upwards_trend:", layout="wide")
 
-# Page title
-st.title(":chart_with_upwards_trend: Stock Price Forecasting")
+def main():
+    # Page config
+    st.set_page_config(page_title="Stock Price Forecasting", page_icon=":chart_with_upwards_trend:", layout="wide")
 
-# Add search input textbox
-symbol = st.sidebar.text_input("Search for Symbols:").upper()
+    # Page title
+    st.title(":chart_with_upwards_trend: Stock Search & Forecasting Web App")
 
-try:
-    # Download stock data from Yahoo Finance
-    stock_data = fcn.fetch_data(symbol)
+    # Add search input textbox
+    symbol = st.sidebar.text_input("Search for Symbols:").upper()
 
-except ValueError:
-    st.error("Please enter a symbol to start!")
+    try:
+        # Download stock data from Yahoo Finance
+        stock_data = fcn.fetch_data(symbol)
 
-else:
-
-    if stock_data.shape[0] == 0:
-        st.error("Symbol not found, please try again!")
+    except ValueError:
+        st.error("Please enter a symbol to start!")
 
     else:
-        # Choose start date
-        start_date = st.sidebar.date_input("Choose Start Date:", value=min(stock_data["Date"]),
-                                           min_value=min(stock_data["Date"]), max_value=max(stock_data["Date"]))
-        # Choose end date
-        end_date = st.sidebar.date_input("Choose End Date:", value=max(stock_data["Date"]),
-                                         min_value=start_date,max_value=max(stock_data["Date"]))
 
-        # Apply date filter to stock data
-        stock_data = stock_data[stock_data["Date"].between(start_date, end_date)]
+        if stock_data.shape[0] == 0:
+            st.error("Symbol not found, please try again!")
 
-        # Display number of data loaded
-        st.sidebar.metric("Data Loaded (Days): ", stock_data.shape[0])
+        else:
+            # Choose start date
+            start_date = st.sidebar.date_input("Choose Start Date:", value=min(stock_data["Date"]),
+                                               min_value=min(stock_data["Date"]), max_value=max(stock_data["Date"]))
+            # Choose end date
+            end_date = st.sidebar.date_input("Choose End Date:", value=max(stock_data["Date"]),
+                                             min_value=start_date,max_value=max(stock_data["Date"]))
 
-        # Show data
-        if st.checkbox("Show Data"):
-            st.write(stock_data)
+            # Apply date filter to stock data
+            stock_data = stock_data[stock_data["Date"].between(start_date, end_date)]
 
-        # Plot candlestick chart
-        fcn.plot_candlestick_chart(symbol, stock_data)
+            # Display number of data loaded
+            st.sidebar.metric("Data Loaded (Days): ", stock_data.shape[0])
 
-        model_options = ["LinearRegression", "RandomForestRegressor", "ExtraTreesRegressor", "KNeighborsRegressor", "XGBoostRegressor"]
-        model_selected = st.selectbox("Choose Model:", model_options)
+            # Show data
+            if st.checkbox("Show Data"):
+                st.write(stock_data)
 
-        num = st.number_input('How many days forecast?', value=5)
+            # Plot candlestick chart
+            fcn.plot_candlestick_chart(symbol, stock_data)
 
-        # Keep only the 'Date' and 'Close' columns for modeling
-        data = stock_data[["Date", "Close"]]
+            model_options = ["LinearRegression", "RandomForestRegressor", "ExtraTreesRegressor", "KNeighborsRegressor", "XGBoostRegressor"]
+            model_selected = st.selectbox("Choose Model:", model_options)
 
-        fcn.predict(model_selected, num, data)
+            num = st.number_input('How many days forecast?', value=5)
 
-        data.columns = ["ds", "y"]  # Prophet requires columns to be named 'ds' and 'y'
+            # Keep only the 'Date' and 'Close' columns for modeling
+            data = stock_data[["Date", "Close"]]
 
-        # Reset index
-        data.reset_index(drop=True, inplace=True)
+            fcn.predict(model_selected, num, data)
 
-        # Initialize and fit the Prophet model
-        model = Prophet(daily_seasonality=True)
-        model.fit(data)
+            data.columns = ["ds", "y"]  # Prophet requires columns to be named 'ds' and 'y'
 
-        # Create a dataframe to hold predictions for the next 90 days
-        future = model.make_future_dataframe(periods=90)
+            # Reset index
+            data.reset_index(drop=True, inplace=True)
 
-        # Generate predictions
-        forecast = model.predict(future)
+            # Initialize and fit the Prophet model
+            model = Prophet(daily_seasonality=True)
+            model.fit(data)
 
-        # Plot the forecasted values
-        model.plot(forecast)
-        plt.title("Tesla Stock Price Prediction")
-        plt.xlabel("Date")
-        plt.ylabel("Close Price (USD)")
-        st.pyplot(plt)
+            # Create a dataframe to hold predictions for the next 90 days
+            future = model.make_future_dataframe(periods=90)
 
-        # Plot the forecast components (trend, weekly, yearly seasonality)
-        model.plot_components(forecast)
-        st.pyplot(plt)
+            # Generate predictions
+            forecast = model.predict(future)
+
+            # Plot the forecasted values
+            model.plot(forecast)
+            plt.title("Tesla Stock Price Prediction")
+            plt.xlabel("Date")
+            plt.ylabel("Close Price (USD)")
+            st.pyplot(plt)
+
+            # Plot the forecast components (trend, weekly, yearly seasonality)
+            model.plot_components(forecast)
+            st.pyplot(plt)
 
 
+if __name__ == '__main__':
+    main()
